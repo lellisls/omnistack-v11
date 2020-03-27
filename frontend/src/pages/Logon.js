7c7375279303613c1e74d7ac210fe6a3ab8934af
Lucas Lellis <lucaslellis777@gmail.com>
2020-03-27 13:56:50 -0300

    Initial setup - frontend

---
+++ frontend/src/pages/Logon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Logon({ children }) {
+  return (
+    <header>
+      <h1>Hello</h1>
+    </header>
+  );
+}
