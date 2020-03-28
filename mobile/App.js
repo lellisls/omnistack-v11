7ff8b776dc55a4407db818345e4fb5dd5202b2f4
Lucas Lellis <lucaslellis777@gmail.com>
2020-03-28 16:16:36 -0300

    Navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello Lucas</Text>
-    </View>
-  );
+  return <Routes />;
 }
